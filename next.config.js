3c048a8f9bc7804967b0a32a958610819b1f1539
mikan3rd <mikan.no.kikoushi@gmail.com>
2023-08-27 22:38:50 +0900

    chore: linariaの導入

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withLinaria = require("next-linaria");
+
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withLinaria(nextConfig);
