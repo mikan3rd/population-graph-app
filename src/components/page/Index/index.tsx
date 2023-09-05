943b9599b2690f63d25a18ab92bf01b5e62094e3
mikan3rd <mikan.no.kikoushi@gmail.com>
2023-09-05 09:07:26 +0900

    chore: RadioButton

--- src/components/page/Index/index.tsx
+++ src/components/page/Index/index.tsx
@@ -5,6 +5,7 @@ import { HighchartsReact } from "highcharts-react-official";
 import { useIndex } from "./index.hook";
 
 import { Checkbox } from "@/components/ui/Checkbox";
+import { RadioButton } from "@/components/ui/RadioButton";
 
 export const Index = () => {
   const {
@@ -26,52 +27,80 @@ export const Index = () => {
 
       <div
         className={css`
-          display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(110px, 1fr));
-          gap: 8px;
-          margin-top: 16px;
+          margin-top: 32px;
         `}
       >
-        {prefectures.map((prefecture) => {
-          const { prefCode, prefName, checked } = prefecture;
-          return (
-            <Checkbox
-              key={prefCode}
-              id={`checkbox_pref_${prefCode}`}
-              checked={checked}
-              value={prefCode}
-              onChange={handleChangeCheckedCode}
-            >
-              {prefName}
-            </Checkbox>
-          );
-        })}
+        <h2
+          className={css`
+            font-size: 18px;
+          `}
+        >
+          都道府県を選択してください
+        </h2>
+        <div
+          className={css`
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(110px, 1fr));
+            gap: 8px;
+            margin-top: 8px;
+          `}
+        >
+          {prefectures.map((prefecture) => {
+            const { prefCode, prefName, checked } = prefecture;
+            return (
+              <Checkbox
+                key={prefCode}
+                id={`checkbox_pref_${prefCode}`}
+                checked={checked}
+                value={prefCode}
+                onChange={handleChangeCheckedCode}
+              >
+                {prefName}
+              </Checkbox>
+            );
+          })}
+        </div>
       </div>
 
       <div
         className={css`
-          margin-top: 16px;
+          margin-top: 32px;
         `}
       >
-        {labels.map((label) => {
-          const { index, name } = label;
-          return (
-            <label key={index}>
-              <input
-                type="radio"
+        <h2
+          className={css`
+            font-size: 18px;
+          `}
+        >
+          表示するデータを選択してください
+        </h2>
+        <div
+          className={css`
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(110px, 1fr));
+            gap: 8px;
+            margin-top: 8px;
+          `}
+        >
+          {labels.map((label) => {
+            const { index, name } = label;
+            return (
+              <RadioButton
+                key={index}
                 value={index}
                 checked={targetDataIndex === index}
                 onChange={handleChangeTargetDataIndex}
-              />
-              {name}
-            </label>
-          );
-        })}
+              >
+                {name}
+              </RadioButton>
+            );
+          })}
+        </div>
       </div>
 
       <div
         className={css`
-          margin-top: 16px;
+          margin-top: 32px;
         `}
       >
         <HighchartsReact highcharts={Highcharts} options={highchartsOptions} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ export default function IndexPage() {
     <>
       <Head>
         <title>population-graph-app</title>
+        <meta name="description" content="人口構成の推移を都道府県別にグラフで確認できるアプリです。" />
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🚀</text></svg>"
