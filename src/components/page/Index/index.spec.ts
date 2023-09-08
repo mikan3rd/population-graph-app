90a636c4bb864466610db4bee8104d6bd5b07bb6
mikan3rd <mikan.no.kikoushi@gmail.com>
2023-09-09 08:50:36 +0900

    chore: add test

--- src/components/page/Index/index.spec.ts
+++ src/components/page/Index/index.spec.ts
@@ -7,24 +7,31 @@ test.describe("Index page", () => {
     await page.goto("/");
   });
 
-  test("render first page", async ({ page }) => {
-    const heading = await page.getByRole("heading", { name: "population-graph-app" });
-    await expect(heading).toBeVisible();
-  });
+  test.describe("initial render", () => {
+    test("render page title", async ({ page }) => {
+      const heading = await page.getByRole("heading", { name: "population-graph-app" });
+      await expect(heading).toBeVisible();
+    });
 
-  test("render checkboxes for 47 prefectures", async ({ page }) => {
-    const heading = await page.getByRole("heading", { name: "都道府県を選択してください（複数可）" });
-    await expect(heading).toBeVisible();
+    test("render checkboxes for 47 prefectures", async ({ page }) => {
+      const heading = await page.getByRole("heading", { name: "都道府県を選択してください（複数可）" });
+      await expect(heading).toBeVisible();
 
-    const checkboxList = await page.$$('[type="checkbox"]');
-    await expect(checkboxList).toHaveLength(47);
-  });
+      const checkboxList = await page.$$('[type="checkbox"]');
+      await expect(checkboxList).toHaveLength(47);
+    });
 
-  test("render chart label radio buttons", async ({ page }) => {
-    const heading = await page.getByRole("heading", { name: "表示するデータを選択してください" });
-    await expect(heading).toBeVisible();
+    test("render chart label radio buttons", async ({ page }) => {
+      const heading = await page.getByRole("heading", { name: "表示するデータを選択してください" });
+      await expect(heading).toBeVisible();
 
-    const radioList = await page.$$('[type="radio"]');
-    await expect(radioList).toHaveLength(4);
+      const radioList = await page.$$('[type="radio"]');
+      await expect(radioList).toHaveLength(4);
+    });
+
+    test("render chart", async ({ page }) => {
+      const chartTitle = await page.locator(".highcharts-title");
+      await expect(chartTitle).toBeVisible();
+    });
   });
 });
