12fbf83006d2fe65e38b38815fe1d7f6ced9cde0
mikan3rd <mikan.no.kikoushi@gmail.com>
2023-09-09 20:49:53 +0900

    fix: undo setupTest

--- src/mocks/setupTest.ts
+++ src/mocks/setupTest.ts
@@ -1,6 +1,17 @@
-import { test } from "next/experimental/testmode/playwright/msw";
+import { test, setupWorker } from "next/experimental/testmode/playwright/msw";
 
 import { handlers } from "./handlers";
+
+async function initMocks() {
+  if (typeof window === "undefined") {
+    // pass
+  } else {
+    const worker = setupWorker(...handlers);
+    worker.start();
+  }
+}
+
+initMocks();
 
 test.use({
   mswHandlers: handlers,
